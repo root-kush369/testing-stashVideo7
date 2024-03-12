eff541d61125b47495be31f4582386d7993c1c73
Kush Bhardwaj <163091134+root-kush369@users.noreply.github.com>
2024-03-12 22:41:04 +0530

    Create main.cpp
    Created main.cpp to know stash concept of Video7

---
+++ main.cpp
@@ -0,0 +1,42 @@
+//sorting concept
+
+#include "iostream"
+// <<<<<<< HEAD
+void sorting1()
+{//
+ 
+}
+// =======
+void sorting2()
+{
+	//
+
+}
+
+// >>>>>>> sorting2
+int main()
+{
+	int arr[5]={0};
+	
+	
+	cout<<"welcome to arr world"<<endl;
+	arr[0]=1;
+	
+	arr[1]=11;
+	arr[2]=111;
+	arr[3]=1111;
+	arr[4]=11111;
+
+	cout<<"simply printing elements of arr";
+
+
+		for(int i =0 ; i<=4 ; ++i)
+		{	
+			cout<<arr[i]<<endl;
+		}
+
+	return 0;
+}
+
+
+
